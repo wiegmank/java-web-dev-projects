9bf74fedb5adbe12dfd3b9297091f7922ed85815
Kevin Wiegman <wiegmank@gmail.com>
2023-12-16 22:18:51 -0600

    unit testing exercises complete

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,10 +1,26 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    Car test_car;
+    @BeforeEach
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
     //TODO: constructor sets gasTankLevel properly
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
